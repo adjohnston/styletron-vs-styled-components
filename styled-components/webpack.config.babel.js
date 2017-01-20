d58bd838f571472e5eeab52586fce809461fae79
Adam Johnston <adam@getkahoot.com>
2017-01-20 14:49:35 +0000

    setup

---
+++ styled-components/webpack.config.babel.js
@@ -0,0 +1,38 @@
+import { resolve } from 'path'
+import { HotModuleReplacementPlugin } from 'webpack'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+
+export default {
+  entry: resolve(__dirname, 'src/index.js'),
+
+  output: {
+    path: resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+
+  dev: {
+    eval: true
+  },
+
+  devServer: {
+    hot: true,
+    inline: true,
+    noInfo: true,
+    port: 8080
+  },
+
+  plugins: [
+    new HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: resolve(__dirname, 'src/index.html')
+    })
+  ],
+
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel'
+    }]
+  }
+}
---
+++ styletron/src/index.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+render(<div>I live</div>, document.querySelector('#app'))
